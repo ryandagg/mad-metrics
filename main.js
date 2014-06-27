60a75e50226fc3b2677b56132707ec404dc2818a
ryandagg <ryan.dagg@gmail.com>
2014-06-26 19:58:11 -0600

    Timer starting on load & stopping on button push

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// timers are likely the easiest and will start there.
+timeCount = 0;
+
+var timer = setInterval(function() {timeCount++; console.log(timeCount)}, 1000);
+
 $(document).on('ready', function() {
-  
+	$(".metricsBtn").on('click', function(){
+		clearTimeout(timer);
+	})
 });
